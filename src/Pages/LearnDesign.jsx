a7537739c0ca7cd4813b1ec86a2c24b7a6057c5e
Felix Oduro Fosu <45005547+Kephass@users.noreply.github.com>
2022-03-10 15:30:18 +0100

    Learn Design page pretty much done.

--- src/Pages/LearnDesign.jsx
+++ src/Pages/LearnDesign.jsx
@@ -1,12 +1,18 @@
 import { Box, Container, Heading } from '@chakra-ui/react';
 import React from 'react';
-import { Banner, Faqs, CoursesBanner } from '@components/learnDesign';
+import {
+  Banner,
+  Faqs,
+  CoursesBanner,
+  WorkshopsBanner,
+} from '@components/learnDesign';
 
 const LearnDesign = () => {
   return (
     <Box>
       <Banner />
       <CoursesBanner />
+      <WorkshopsBanner />
       <Box bgColor="bgFAQ">
         <Container maxW="container.lg" py="4rem">
           <Heading as="h1" size="xl" lineHeight="56px" mb="2rem">
--- src/components/learnDesign/WorkshopsBanner.jsx
+++ src/components/learnDesign/WorkshopsBanner.jsx
@@ -1,7 +1,95 @@
-import React from 'react';
+import {
+  Container,
+  Stack,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Image,
+} from '@chakra-ui/react';
 
-const workshopsBanner = () => {
-  return <div>workshopsBanner</div>;
+export const WorkshopsBanner = () => {
+  return (
+    <Container maxW={'7xl'}>
+      <Heading as="h1" align="center">
+        Upcoming certified courses
+      </Heading>
+      <Stack
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 17, md: 24 }}
+        direction={{ base: 'column', md: 'row' }}
+      >
+        <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+          <Heading
+            lineHeight={1.1}
+            fontWeight={600}
+            fontSize={{ base: '3xl', sm: '4xl', lg: '4xl' }}
+          >
+            <Text
+              as={'span'}
+              position={'relative'}
+              fontSize="md"
+              color={'purple.600'}
+            >
+              Wed, March 9, 2022 & Wed, March 16, 2022
+            </Text>
+            <br />
+            <Text as={'span'}>
+              Designer/Developer Collaboration with Brad Frost & Dan Mall
+            </Text>
+            <br />
+            <Text as={'span'} fontSize="md" color={'siteGray'}>
+              Taught by Brad Frost & Dan Mall
+            </Text>
+          </Heading>
+          <Text>
+            Join world renowned designers and developers Brad Frost & Dan Mall
+            for an exclusive two-part interactive workshop on designer &
+            developer collaboration.
+          </Text>
+          <Flex gap="4">
+            <Button
+              fontSize=".9rem"
+              alignSelf="flex-start"
+              bgColor="#4bb7bf"
+              color="white"
+              _hover={{
+                backgroundColor: 'cyan.700',
+              }}
+            >
+              Register now
+            </Button>
+            <Button fontSize=".9rem" alignSelf="flex-start">
+              Learn more
+            </Button>
+          </Flex>
+        </Stack>
+        <Flex
+          flex={1}
+          justify={'center'}
+          align={'center'}
+          position={'relative'}
+          w={'full'}
+        >
+          <Box
+            position={'relative'}
+            height={'345px'}
+            rounded={'2xl'}
+            boxShadow={'2xl'}
+            width={'full'}
+            overflow={'hidden'}
+          >
+            <Image
+              width="100%"
+              height="100%"
+              objectFit="fill"
+              src="/images/learn-design/workshop-image.png"
+            />
+          </Box>
+        </Flex>
+      </Stack>
+    </Container>
+  );
 };
-
-export default workshopsBanner;
