0bb7e513a1a0cd10acaa9834be8b84e523a9ec8f
Andres Vergauwen <andres.vergauwen@hotmail.com>
2022-03-31 10:45:24 +0200

    feat: show shot modal on each page

--- src/components/landing/Body.jsx
+++ src/components/landing/Body.jsx
@@ -1,17 +1,9 @@
 import { useEffect, useState } from 'react';
-import { AnimatePresence } from 'framer-motion';
 import { useParams } from 'react-router-dom';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import {
-  Button,
-  Center,
-  Container,
-  Flex,
-  Grid,
-  Image,
-  Spinner,
-} from '@chakra-ui/react';
-import { Card, CardText, ModalNoUser } from '@components';
+
+import { Button, Center, Container, Image, Spinner } from '@chakra-ui/react';
+import { ModalNoUser } from '@components';
 import { FilterNav } from '@components/landing';
 import { allPostsStateAtom, userLogInModal } from '@data/atoms';
 
@@ -20,13 +12,11 @@ import {
   getNewPost,
   limitNumber,
 } from '../../firestore.collections';
-import Modal from '../ModalPost';
+import { Shots } from '../userProfile/Shots';
 
 export function Body() {
   // Variables
   let { tag, filter } = useParams();
-  const [modalOpen, setModalOpen] = useState(false);
-  const [selectedPost, setSelectedPost] = useState({});
   const [posts, setPosts] = useRecoilState(allPostsStateAtom);
   const logInModal = useRecoilValue(userLogInModal);
   const [moreShots, setMoreShots] = useState(false);
@@ -59,14 +49,6 @@ export function Body() {
     });
   };
 
-  const close = (post) => {
-    setModalOpen(false);
-    setSelectedPost(post);
-  };
-  const open = (post) => {
-    setModalOpen(true);
-    setSelectedPost(post);
-  };
   const checkMenuItemLabel = (f) => {
     switch (f) {
       case 'popular':
@@ -80,47 +62,9 @@ export function Body() {
   return (
     <Container maxW="95%" overflow="hidden">
       <FilterNav tag={tag} filter={checkMenuItemLabel(filter)} />
-      <Grid
-        py="32px"
-        gap="10"
-        templateColumns={{
-          base: 'repeat(auto-fill, minmax(250px, 1fr))',
-          md: 'repeat(auto-fill, minmax(350px, 1fr))',
-        }}
-      >
-        {posts.map((post, i) => (
-          <Flex
-            width="100%"
-            direction="column"
-            gap="2"
-            key={`${post.docId}${i}`}
-            onClick={() => (modalOpen ? close(post) : open(post))}
-          >
-            <Card
-              item={post}
-              id={post.docId}
-              height="250px"
-              objectFit="cover"
-              setPosts={setPosts}
-            />
-            <CardText item={post} id={post.docId} />
-          </Flex>
-        ))}
-      </Grid>
+      <Shots posts={posts} setPosts={setPosts} />
       {logInModal && <ModalNoUser />}
-      <AnimatePresence
-        initial={false}
-        exitBeforeEnter={true}
-        onExitComplete={() => null}
-      >
-        {modalOpen && (
-          <Modal
-            modalOpen={modalOpen}
-            handleClose={close}
-            item={selectedPost}
-          />
-        )}
-      </AnimatePresence>
+
       {moreShots && (
         <Center my="2em">
           <Button onClick={handleNewPosts}>
--- src/components/userProfile/Shots.jsx
+++ src/components/userProfile/Shots.jsx
@@ -1,16 +1,40 @@
+import { useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
+
 import { Container, Flex, Grid } from '@chakra-ui/react';
 import { Card, CardText } from '@components';
 
+import Modal from '../ModalPost';
+
 export const Shots = ({ posts, setPosts }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedPost, setSelectedPost] = useState({});
+  const close = (post) => {
+    setModalOpen(false);
+    setSelectedPost(post);
+  };
+  const open = (post) => {
+    setModalOpen(true);
+    setSelectedPost(post);
+  };
   return (
     <Container maxW="100%" my="0" p="0">
       <Grid
         py="32px"
         gap="10"
-        templateColumns="repeat(auto-fill, minmax(334px, 1fr))"
+        templateColumns={{
+          base: 'repeat(auto-fill, minmax(250px, 1fr))',
+          md: 'repeat(auto-fill, minmax(350px, 1fr))',
+        }}
       >
         {posts.map((post, i) => (
-          <Flex direction="column" gap="2" key={`shots-${i}`}>
+          <Flex
+            width="100%"
+            direction="column"
+            gap="2"
+            key={`${post.docId}${i}`}
+            onClick={() => (modalOpen ? close(post) : open(post))}
+          >
             <Card
               item={post}
               id={post.docId}
@@ -22,6 +46,20 @@ export const Shots = ({ posts, setPosts }) => {
           </Flex>
         ))}
       </Grid>
+      <AnimatePresence
+        initial={false}
+        exitBeforeEnter={true}
+        onExitComplete={() => null}
+      >
+        {modalOpen && (
+          <Modal
+            modalOpen={modalOpen}
+            handleClose={close}
+            item={selectedPost}
+            setPosts={setPosts}
+          />
+        )}
+      </AnimatePresence>
     </Container>
   );
 };
